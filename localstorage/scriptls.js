2e3a39070de32a63968697ecae787d2fb8f875ca
anna0401 <mfadhilanhar_2428240158@mhs.mdp.ac.id>
2026-04-14 12:50:09 +0700

    commit 5

--- localstorage/scriptls.js
+++ localstorage/scriptls.js
@@ -40,26 +40,18 @@ function tampil() {
     //lakukkan perulangan (foreach)
     hasil.forEach(element => {
         //console.log(element)
-        document.getElementById("list-barang").innerHTML += `<div class="col-lg-4 col-md-6"> <img src="${element.ImageURL}" class="img-fluid"> <h4 class="text-primary">${element.NamaBarang}</h4> <h6 class="text-danger">${element.JumlahBarang}</h6> </div>`
+        document.getElementById("list-barang").innerHTML += `<div class="col-lg-3 col-md-5">
+  <img src="${element.ImageURL}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="text-primary">${element.NamaBarang}</h5>
+    <p class="text-danger">${element.JumlahBarang}</p>
+    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+  </div>
+</div>`
         
     });
 
 }
-<div class="card" style="width: 18rem;">
-  <img src="${element.ImageURL}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
+
 //Jalankan function tampil()
 tampil()
